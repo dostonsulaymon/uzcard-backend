c50a1d86164a1d381c8763ca02abb01a80ba0a04
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-12-09 14:52:41 +0500

    Initial commit

---
+++ src/main/java/dasturlash/uz/UzcardBackendApplication.java
@@ -0,0 +1,13 @@
+package dasturlash.uz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzcardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UzcardBackendApplication.class, args);
+    }
+
+}
